574abb8c1f3111bec7d2f7cfcc7cea2f83b31c4a
dmitri_shepelev <shepelev.dm@gmail.com>
2020-12-19 21:01:40 +0300

    Done

--- TransformerToWords/Transformer.cs
+++ TransformerToWords/Transformer.cs
@@ -1,4 +1,10 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#pragma warning disable CA1822
+#pragma warning disable CA1305
+#pragma warning disable CA1304
 
 namespace TransformerToWords
 {
@@ -19,7 +25,75 @@ namespace TransformerToWords
         /// </example>
         public string[] Transform(double[] source)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} cannot be null.");
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(source)} cannot be empty.");
+            }
+
+            var result = new string[source.Length];
+            for (int i = 0; i < source.Length; i++)
+            {
+                result[i] = TransformToWords(source[i]);
+            }
+
+            return result;
+        }
+
+        private static string TransformToWords(double number)
+        {
+            if (double.IsNaN(number))
+            {
+                return "Not a Number";
+            }
+
+            if (double.IsPositiveInfinity(number))
+            {
+                return "Positive Infinity";
+            }
+
+            if (double.IsNegativeInfinity(number))
+            {
+                return "Negative Infinity";
+            }
+
+            if (number == double.Epsilon)
+            {
+                return "Double Epsilon";
+            }
+
+            var dict = new Dictionary<char, string>()
+            {
+                { '-', "Minus" },
+                { '0', "zero" },
+                { ',', "point" },
+                { '.', "point" },
+                { '1', "one" },
+                { '2', "two" },
+                { '3', "three" },
+                { '4', "four" },
+                { '5', "five" },
+                { '6', "six" },
+                { '7', "seven" },
+                { '8', "eight" },
+                { '9', "nine" },
+                { 'E', "E" },
+                { '+', "plus" },
+            };
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < number.ToString().Length; i++)
+            {
+                sb.Append(dict[number.ToString()[i]]);
+                sb.Append(" ");
+            }
+
+            sb[0] = char.ToUpper(sb[0]);
+            return new string(sb.ToString().TrimEnd());
         }
     }
 }
